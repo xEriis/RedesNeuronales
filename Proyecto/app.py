2ff2cc2a138be7d7eeb3b44adbd7c08d4a438333
AylenMartinez04 <aylenmartinez@ciencias.unam.mx>
2025-12-08 21:42:31 -0600

    comentario sobre prediccion

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -6,9 +6,6 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 
-# load train model
-
-
 # Configuración de la página
 st.set_page_config(
     page_title="Identificador de firmas falsas",
@@ -86,7 +83,7 @@ def get_transformaciones():
         transforms.ToTensor(),
     ])
 
-# Se usa para calcular las predicciones? Se uso Constrstive Loss como función de pérdida
+# Se usa para calcular las predicciones (al igual que lo hicimos el en .ipynb)
 def calcular_distancia(emb1, emb2):
     distancia = nn.functional.pairwise_distance(emb1, emb2)
     return distancia.item()
@@ -104,11 +101,6 @@ def calcular_confianza(distancia, threshold):
         Confianza entre 0 y 1
     """
     temp = threshold / 2
-    
-    # Sigmoid centrado en el threshold
-    # Si distancia = threshold -> confianza ≈ 0.5
-    # Si distancia = 0 -> confianza ≈ 1.0
-    # Si distancia >> threshold -> confianza ≈ 0.0
     logit = -(distancia - threshold) / temp
     confianza = 1 / (1 + np.exp(-logit))
     return confianza
